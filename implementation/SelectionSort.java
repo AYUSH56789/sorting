3157e0869022c0ff608c8fd8a3ea7017e20b0e9e
Ayush Singh <ayushsingh46026@gmal.com>
2024-06-04 11:09:17 +0530

    done selection sort

--- implementation/SelectionSort.java
+++ implementation/SelectionSort.java
@@ -1,5 +1,34 @@
-package implementation;
-
-public class SelectionSort {
-    
+public class SelectionSort{
+    // select min
+    public static int select(int arr[],int i){
+        int min=i;
+        for (int j = i; j <= arr.length-1; j++) {
+            if(arr[j]<arr[min]){
+                min=j;
+            }
+        }
+        return min;
+    }
+    public static void selectionSort(int arr[]){
+        for (int i = 0; i <= arr.length-2; i++) {
+            // select min
+            int min=select(arr, i);
+            // swap arr[min] with arr[i]
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,1,3,2,13};
+        // call selection sort;
+        selectionSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
 }
+
+// input:1 2 3 1 3 2 13
+// output:1 1 2 2 3 3 13 
+// time complexity: O(N^2) -> for best case, average case, worst case time complexity
